109cfa0b3ba4958bacf1061ab6eb45b8a4623716
Lauren Hutchinson <lbhutchinson022@gmail.com>
2021-04-22 09:35:43 -0700

    ADDED: connection made to mongodb

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const path = require("path");
+
+require("./models/connection");
+
+// setup express
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+}
+
+//custom routes
+
+// heroku build
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(PORT, () => console.log(`Listening at: http://localhost:${PORT}`));
